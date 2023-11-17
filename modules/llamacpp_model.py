c21f1241f5aeab18d5dbfac39e57466f8138b052
mint <imbroglio@inbox.lv>
2023-11-17 10:01:02 -0600

    remove seed again

--- modules/llamacpp_model.py
+++ modules/llamacpp_model.py
@@ -54,7 +54,6 @@ class LlamaCppModel:
         params = {
             'model_path': str(path),
             'n_ctx': shared.args.n_ctx,
-            'seed': int(shared.args.llama_cpp_seed),
             'n_threads': shared.args.threads or None,
             'n_threads_batch': shared.args.threads_batch or None,
             'n_batch': shared.args.n_batch,
